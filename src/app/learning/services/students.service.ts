c3abd5fb9ad818ab67bbd67f74c67a19ffa9eccc
JulianaYS <u20201b782@upc.edu.pe>
2023-10-02 04:33:38 -0500

    chore: added http client module

---
+++ src/app/learning/services/students.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {BaseService} from "../../shared/services/base.service";
+import {Student} from "../model/student";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentsService extends BaseService<Student> {
+  constructor(http: HttpClient) {
+    super(http);
+    this.resourceEndpoint = '/students';
+  }
+}
